7cbbafc826fc96f5ccd0494e35a92532b9a8195c
iAKM <achyutkumar.m@gmail.com>
2015-09-14 08:35:22 +0530

    Initial Commit

---
+++ DeprocastinatorCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeprocastinatorCoreData
+//
+//  Created by Achyut Kumar Maddela on 14/09/15.
+//  Copyright (c) 2015 iAKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeprocastinatorCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DeprocastinatorCoreData
+//
+//  Created by Achyut Kumar Maddela on 14/09/15.
+//  Copyright (c) 2015 iAKM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
